ca110ec4b80af5ba8ea035d39089e34918f12674
Rodrigo Falcão <rmfalcao@ig.com.br>
2019-12-18 20:15:35 +0100

    remover auditoria do estorno sistemico de lancamentos duplicados

--- SisjufNovo/apps/app_web_sisjuf/src/br/org/asserjuf/sisjuf/financeiro/dados/LancamentoDAO.java
+++ SisjufNovo/apps/app_web_sisjuf/src/br/org/asserjuf/sisjuf/financeiro/dados/LancamentoDAO.java
@@ -197,17 +197,6 @@ public class LancamentoDAO extends SisjufDAOPostgres {
 			sStmt.getMyPreparedStatement().execute();
 			
 			
-			// o update abaixo visa apenas atualizar os dados de auditoria na tabela LANCAMENTO.
-			// desta forma, o usuario logado e a data e hora atual serao gravados nos campos
-			// seq_usuario_alteracao e dat_alteracao antes do delete. Quando o delete for
-			// realizado, a tabela lancamento_log terah a informacao das colunas preservada.
-						
-			sql = new StringBuffer("UPDATE LANCAMENTO SET SEQ_USUARIO_ALTERACAO = ?, DAT_ALTERACAO = NOW() WHERE seq_lancamento = ? ");
-			sStmt 	= new SmartPreparedStatement(sConn.prepareStatement(sql.toString()));
-			sStmt.setInteger(1, vo.getUsuario().getCodigo());
-			sStmt.setInteger(2, vo.getCodigo());
-			sStmt.getMyPreparedStatement().execute();
-			
 			
 			
 			sql = new StringBuffer("DELETE FROM lancamento WHERE seq_lancamento = ? ");
